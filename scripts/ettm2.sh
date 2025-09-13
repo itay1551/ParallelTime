0de48b67ee643b68d8d9d6c2a78994debe2ac704
Itay <itaykatavv@gmail.com>
2025-09-13 13:21:39 +0300

    chore: migrate to uv
    Signed-off-by: Itay <itaykatavv@gmail.com>

--- scripts/ettm2.sh
+++ scripts/ettm2.sh
@@ -1,4 +1,4 @@
 model_name=ParallelTime
 dataset_name=ettm2
 
-python run_hydra.py --config-name $dataset_name -m pred_len=96,192,336,720
+uv run run_hydra.py --config-name $dataset_name -m pred_len=96,192,336,720
--- scripts/illness.sh
+++ scripts/illness.sh
@@ -1,4 +1,4 @@
 model_name=ParallelTime
 dataset_name=illness
 
-python run_hydra.py --config-name $dataset_name -m pred_len=24,36,48,60
+uv run run_hydra.py --config-name $dataset_name -m pred_len=24,36,48,60
